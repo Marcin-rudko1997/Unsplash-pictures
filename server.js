cd88658522818256e20886cfcd66c2d21b2e9d60
Marcin <marcin-rudko1997@wp.pl>
2019-05-09 22:04:08 +0200

    finish

--- server.js
+++ server.js
@@ -4,11 +4,9 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 
-app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
-console.log('hello');
